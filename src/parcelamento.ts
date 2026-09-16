4387a8a227d0a39c2fe26653e7ca3db9e6615793
barbaramazevedo <barbara.azevedo22@gmail.com>
2026-09-16 10:40:22 -0300

    feat: implementa a função com juros 5 porcento

--- src/parcelamento.ts
+++ src/parcelamento.ts
@@ -7,7 +7,8 @@ export function calcularParcelamento(valor:number, qtdParcelas:number): number {
 
     if (qtdParcelas <= 4) {
         return valor / qtdParcelas
+    } else if (qtdParcelas >= 5 && qtdParcelas <=8) {
+         return (valor * 1.05) / qtdParcelas
     }
-
     return valor
 }
