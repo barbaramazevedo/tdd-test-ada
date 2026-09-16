2da17143d9b95deba99742f40901e730460dfd89
barbaramazevedo <barbara.azevedo22@gmail.com>
2026-09-16 10:34:27 -0300

    test: implementa test com juros 5%

--- src/parcelamento.test.ts
+++ src/parcelamento.test.ts
@@ -5,26 +5,35 @@ describe('calcularParcelamento', () => {
     describe('sem juros (1x a 4x)', () => {
         it('retorna o valor total em parcela única quando for 1x', () => {
             //Act
-            const valorTotalParcelaUnica = calcularParcelamento(100, 1)
+            const valorTotalParcelaUnica = calcularParcelamento(1000, 1)
     
             //Assert
-            expect(valorTotalParcelaUnica).toBe(100)
+            expect(valorTotalParcelaUnica).toBe(1000)
         })
         
         it('divide o valor sem juros quando for 4x', () => {
             //Act
-            const valorParcelaQuatro = calcularParcelamento(100, 4)
+            const valorParcelaQuatro = calcularParcelamento(1000, 4)
     
             //Assert
-            expect(valorParcelaQuatro).toBe(25)
+            expect(valorParcelaQuatro).toBe(250)
         })
     })
 
     describe('com juros', () => {
-        it('aplica 5% sobre o total quando for de 5x a 8x')
-        it('aplica 8% sobre o total quando for de 9x a 12x')
-        it('aplica 10% sobre o total quando for de 13x a 18x')
-        it('aplica a faixa correta nos limites (4x, 5x, 8x, 9x, 12x, 13x)')
+        it('aplica 5% sobre o total quando for de 5x a 8x', () => {
+            const valorParcelaOito = calcularParcelamento(1000, 8)
+            expect(valorParcelaOito).toBe(131.25)
+        })
+
+        it('aplica 8% sobre o total quando for de 9x a 12x', () => {
+        })
+
+        it('aplica 10% sobre o total quando for de 13x a 18x', () => {
+        })
+
+        it('aplica a faixa correta nos limites (4x, 5x, 8x, 9x, 12x, 13x)', () => {
+        })
     })
 
     describe('arredondamento', () => {
