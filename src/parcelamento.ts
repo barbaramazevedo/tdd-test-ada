8624eb73e6d54a3f4184806b3bf12e8c1a34dbae
barbaramazevedo <barbara.azevedo22@gmail.com>
2026-09-16 11:36:45 -0300

    feat: implementa validações de erro

--- src/parcelamento.ts
+++ src/parcelamento.ts
@@ -4,18 +4,26 @@ export type Parcelamento = {
 }
 
 export function calcularParcelamento(valor:number, qtdParcelas:number): number {
-    let resultado : number
+    if (!Number.isInteger(qtdParcelas))
+        throw new Error('Erro: número de parcelas tem que ser um número inteiro')
+    if (qtdParcelas < 1)
+        throw new Error('Erro: número de parcelas menor que 1')
+    if (qtdParcelas > 18)
+        throw new Error('Erro: número de parcelas maior que 18')
+    if (valor <= 0)
+        throw new Error('Erro: valor da compra tem que ser maior que zero')
+
+    let resultado: number
 
     if (qtdParcelas <= 4) {
         resultado = valor / qtdParcelas
-    } else if (qtdParcelas >= 5 && qtdParcelas <= 8) {
+    } else if (qtdParcelas <= 8) {
         resultado = (valor * 1.05) / qtdParcelas
-    } else if (qtdParcelas >= 9 && qtdParcelas <= 12) {
+    } else if (qtdParcelas <= 12) {
         resultado = (valor * 1.08) / qtdParcelas
-     } else {
+    } else {
         resultado = (valor * 1.10) / qtdParcelas
     }
-
 
     return Math.round(resultado * 100) / 100
 }
