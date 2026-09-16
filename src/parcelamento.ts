411b01b22efb4a4fc48f839402b100f4bde7b00c
barbaramazevedo <barbara.azevedo22@gmail.com>
2026-09-16 11:01:54 -0300

    feat: implementa a função com os calculos dos juros

--- src/parcelamento.ts
+++ src/parcelamento.ts
@@ -4,11 +4,18 @@ export type Parcelamento = {
 }
 
 export function calcularParcelamento(valor:number, qtdParcelas:number): number {
+    let resultado : number
 
     if (qtdParcelas <= 4) {
-        return valor / qtdParcelas
-    } else if (qtdParcelas >= 5 && qtdParcelas <=8) {
-         return (valor * 1.05) / qtdParcelas
+        resultado = valor / qtdParcelas
+    } else if (qtdParcelas >= 5 && qtdParcelas <= 8) {
+        resultado = (valor * 1.05) / qtdParcelas
+    } else if (qtdParcelas >= 9 && qtdParcelas <= 12) {
+        resultado = (valor * 1.08) / qtdParcelas
+     } else {
+        resultado = (valor * 1.10) / qtdParcelas
     }
-    return valor
+
+
+    return Math.round(resultado * 100) / 100
 }
